0a85f8518e4ec5cc1e8a0f25004182c5792e9a31
hanekensama <is0182vp@ed.ritsumei.ac.jp>
2017-10-13 16:13:50 +0900

    Update 08_git.el

--- common/08_git.el
+++ common/08_git.el
@@ -5,7 +5,9 @@
   :defer t
   :config
   (setq-default magit-auto-revert-mode nil)
-  (setq vc-handled-backends '()))
+  (setq vc-handled-backends '())
+  :bind
+  (("C-x g" . magit-status)))
 
 (use-package git-gutter-fringe+)
 
