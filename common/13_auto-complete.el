a4890b305e5fb4019a49819014f6f8a15ed6ecce
hanekensama <hanekensama@gmail.com>
2017-10-19 01:24:48 +0900

    company and irony doesn't work

--- common/13_auto-complete.el
+++
@@ -1,8 +0,0 @@
-(use-package auto-complete
-  :diminish auto-complete-mode
-  :config
-  (add-to-list 'ac-dictionary-directories (locate-user-emacs-file "./ac-dict"))
-  (use-package auto-complete-config)
-  (ac-config-default)
-  (global-auto-complete-mode t)
-  )
