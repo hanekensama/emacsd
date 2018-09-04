aec0c278d92d85d20e0999dc55e3991a527f6cbb
Kentaro Haneda <hanekensama@gmail.com>
2018-09-04 19:28:31 +0900

    configured auto completion for c/c++

--- common/16_company.el
+++ common/16_company.el
@@ -3,7 +3,10 @@
   :init
   (global-company-mode t)
   (global-set-key (kbd "C-M-i") 'company-complete)
-  (setq company-idle-delay 0.2)
+  (setq company-idle-delay 0)
+  (setq company-echo-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
 
   :config
   (bind-keys :map company-active-map
--- common/20_cc-mode.el
+++ common/20_cc-mode.el
@@ -11,4 +11,29 @@
     (setq c-basic-offset 2)
     (setq c-auto-newline t) ; {や;などを入力すると自動で改行
     (electric-pair-mode t)
+    (use-package irony
+      :ensure t
+      :defer t
+      :init
+      (setq irony-additional-clang-options '("-std=c++14"))
+      (use-package company-irony
+        :ensure t
+        :defer t
+        :init
+        (add-to-list 'company-backends 'company-irony))
+      (use-package company-irony-c-headers
+        :ensure t
+        :defer t
+        )
+      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+      (add-hook 'c-mode-hook 'irony-mode)
+      (add-hook 'c++-mode-hook 'irony-mode)
+      (use-package c-eldoc
+        :ensure t
+        :defer t
+        :config
+        (setq c-eldoc-buffer-regenerate-time 60)
+        )
+      (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+      (add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
     )
