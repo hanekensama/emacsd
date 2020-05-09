dcb14753e8f9777009c2134686bebf482ad60b6a
Kentaro Haneda <hanekensama@gmail.com>
2020-05-09 16:04:27 +0900

    新規作成

---
+++ templates/cpp-template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+// マクロ
+/// ループ
+#define FOR(i,a,b) for(ll i=a;i<=(ll)(b);++i)
+#define FORD(i,a,b) for(ll i=a;i>=(ll)(b);--i)
+#define REP(i,n) FOR(i,0,n) 
+#define REPD(i,n) FORD(i,n,0)
+/// コンテナ用
+#define ALL(x) (x).begin(),(x).end()
+#define SIZE(x) ((ll)(x).size())
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
+/// 定数
+#define INF 1000000000000 //10^12
+#define MOD 1000000007 //10^9+7
+#define MAXR 100000 //10^5 配列の最大のRANGE
+/// 略記
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+
+auto main() -> signed {
+}
